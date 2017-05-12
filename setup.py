1c4365cf8c334f0cf60d0fefdb4a2a6bc5e21fda
greg <99hilmes.g@gmail.com>
2017-05-12 17:33:26 -0500

    fixed PyPI download url

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='pokebase',
   packages=['pokebase'],
-  version='1.0',
+  version='v1.0',
   description='A Python wrapper for the friendly PokeAPI database',
   author='Greg Hilmes',
   author_email='99hilmes.g@gmail.com',
   url='https://github.com/GregHilmes/pokebase',
-  download_url='https://github.com/GregHilmes/pokebase/archive/1.0.tar.gz',
+  download_url='https://github.com/GregHilmes/pokebase/archive/v1.0.tar.gz',
   keywords=['database', 'pokemon', 'wrapper'],
   classifiers=[],
 )
