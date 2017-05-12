589d8ce9ad79668e2c9b35a5a540fb5be13159a3
greg <99hilmes.g@gmail.com>
2017-05-12 18:37:24 -0500

    added classifiers

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from distutils.core import setup
 setup(
     name='pokebase',
     packages=['pokebase'],
-    version='1.0',
+    version='1.0.0',
     description='A Python wrapper for the friendly PokeAPI database',
     author='Greg Hilmes',
     author_email='99hilmes.g@gmail.com',
     url='https://github.com/GregHilmes/pokebase',
-    download_url='https://github.com/GregHilmes/pokebase/archive/1.0.tar.gz',
+    download_url='https://github.com/GregHilmes/pokebase/archive/1.0.0.tar.gz',
     keywords=['database', 'pokemon', 'wrapper'],
     install_requires=['requests'],
+    license='BSD-3-Clause',
+    classifiers=[
+        'license :: BSD 3 Clause License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
+    ]
 )
