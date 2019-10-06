df6db21a0fafebb9efbdfe72626c58f1499d93e6
Neetansh <56220285+Neetansh@users.noreply.github.com>
2019-10-06 23:23:40 +0530

    Delete factorial.py

--- factorial.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter any number: "))
-flag = num%2
-if flag == 0:
-    print(num, "is an even number")
-elif flag == 1:
-    print(num, "is an odd number")
-else:
-    print("Error, Invalid input")
